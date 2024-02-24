c0cce871c583f743e4c1c0ad464315e690fbe9de
Devanshi Sahu <98383396+devanshisahu@users.noreply.github.com>
2024-02-24 15:58:00 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,53 @@
+$('.slick-slider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+ 
+    // ______________ Back to Top js
+    $(window).on("scroll", function (e) {
+        if ($(this).scrollTop() > 0) {
+            $('#back-to-top').fadeIn('slow');
+        } else {
+            $('#back-to-top').fadeOut('slow');
+        }
+    });
+    $("#back-to-top").on("click", function (e) {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
